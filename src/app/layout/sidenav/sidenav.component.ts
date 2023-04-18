0e416663898fb19e15e58b292d388566049cdc30
Andrés Heredia Canales <heredia@ifca.unican.es>
2023-04-18 19:02:35 +0200

    Hide sidenav menu items when not authenticated

--- src/app/layout/sidenav/sidenav.component.ts
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { AuthService } from '@app/core/services/auth/auth.service';
 
 
 @Component({
@@ -7,47 +8,57 @@ import { FormBuilder } from '@angular/forms';
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent  {
+export class SidenavComponent {
   options = this._formBuilder.group({
-  bottom: 0,
-  fixed: false,
-  top: 0,
-});
+    bottom: 0,
+    fixed: false,
+    top: 0,
+  });
 
-constructor(private _formBuilder: FormBuilder) {}
+  constructor(
+    private _formBuilder: FormBuilder,
+    protected authService: AuthService
+  ) { }
 
-mainLinks = [
-  {
-    name: "SIDENAV.DASHBOARD",
-    url: "/"
-  },
-  {
-    name: "SIDENAV.MODULES",
-    url: "/modules"
-  },
-  {
-    name: "SIDENAV.DEPLOYMENTS",
-    url: "/deployments"
-  } 
-]
+  mainLinks = [
+    {
+      name: "SIDENAV.DASHBOARD",
+      url: "/",
+      isRestricted: true
+    },
+    {
+      name: "SIDENAV.MODULES",
+      url: "/modules"
+    },
+    {
+      name: "SIDENAV.DEPLOYMENTS",
+      url: "/deployments",
+      isRestricted: true
+    }
+  ]
 
-otherLinks = [
-  {
-    name: "SIDENAV.IAM",
-    url: "https://iam.deep-hybrid-datacloud.eu"
-  },
-  {
-    name: "SIDENAV.DEEP-MARKETPLACE",
-    url: "https://marketplace.deep-hybrid-datacloud.eu"
-  },
-  {
-    name: "SIDENAV.DEEP-DOCUMENTATION",
-    url: "https://docs.deep-hybrid-datacloud.eu"
-  },
-  {
-    name: "SIDENAV.PROJECT-PAGE",
-    url: "https://deep-hybrid-datacloud.eu"
+  otherLinks = [
+    {
+      name: "SIDENAV.IAM",
+      url: "https://iam.deep-hybrid-datacloud.eu"
+    },
+    {
+      name: "SIDENAV.DEEP-MARKETPLACE",
+      url: "https://marketplace.deep-hybrid-datacloud.eu"
+    },
+    {
+      name: "SIDENAV.DEEP-DOCUMENTATION",
+      url: "https://docs.deep-hybrid-datacloud.eu"
+    },
+    {
+      name: "SIDENAV.PROJECT-PAGE",
+      url: "https://deep-hybrid-datacloud.eu"
+    }
+  ]
+
+  isLoggedIn(): boolean {
+    return this.authService.isAuthenticated();
   }
-]
+
 
 }
