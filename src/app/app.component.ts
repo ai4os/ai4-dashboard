96f0a64488c7083badfd6daaea8de77b4b74b45b
Andrés Heredia Canales <heredia@ifca.unican.es>
2023-04-28 13:37:29 +0200

    feat: change title of app based of config file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { AppConfigService } from './core/services/app-config/app-config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ai4-dashboard';
+
+  constructor(
+    private titleService: Title,
+    private appConfigService: AppConfigService
+    ) {}
+  
+ ngOnInit(): void {
+  this.titleService.setTitle(this.appConfigService.title);
+}
 }
