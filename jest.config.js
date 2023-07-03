ba39a09f0f73db6869da8474d2452fca33dd8873
Andrés Heredia Canales <heredia@ifca.unican.es>
2023-07-03 13:30:26 +0200

    Fix jest not finding modules

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,17 @@ module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
     globalSetup: 'jest-preset-angular/global-setup',
+    modulePaths: [
+        "<rootDir>",
+        "/home/some/other/path"
+      ],
+    moduleNameMapper: {
+        "^~(.*)$": "<rootDir>/src/$1",
+        "@app(.*)": "<rootDir>/src/app/$1",
+        "@core(.*)": "<rootDir>/src/app/core/$1",
+        "@modules(.*)": "<rootDir>/src/app/modules/$1",
+        "@shared(.*)": "<rootDir>/src/app/shared/$1",
+        "@environments(.*)": "<rootDir>/src/environments/$1",
+        "@data(.*)": "<rootDir>/src/app/data/$1",
+      }
 };
