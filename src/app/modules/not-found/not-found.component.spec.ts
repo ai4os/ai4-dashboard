e8db017b87c342f95bcfd34c55f5497476b690a3
Andrés Heredia Canales <heredia@ifca.unican.es>
2023-12-18 10:47:49 +0100

    test: Add missing shared module to not-found module

--- src/app/modules/not-found/not-found.component.spec.ts
+++ src/app/modules/not-found/not-found.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NotFoundComponent } from './not-found.component';
 import { By } from '@angular/platform-browser';
 import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '@app/shared/shared.module';
 
 describe('NotFoundComponent', () => {
     let component: NotFoundComponent;
@@ -10,7 +11,7 @@ describe('NotFoundComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [TranslateModule.forRoot()],
+            imports: [TranslateModule.forRoot(), SharedModule],
             declarations: [NotFoundComponent],
         }).compileComponents();
 
