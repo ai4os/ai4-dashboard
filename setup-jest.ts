14d12b2c007ebbb4ffcd26c530e8da37d1d3785b
Andrés Heredia Canales <heredia@ifca.unican.es>
2023-12-18 10:47:20 +0100

    feat: Add jest-fail-on-console module for better testing

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,1 +1,10 @@
 import 'jest-preset-angular/setup-jest';
+
+import failOnConsole from 'jest-fail-on-console';
+
+failOnConsole();
+
+// or with options:
+failOnConsole({
+    shouldFailOnWarn: false,
+});
