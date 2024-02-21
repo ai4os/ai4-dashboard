755285c138dae81a850637a05c97043fd0f30b6e
Marta Obregón <obregonm@ifca.unican.es>
2024-02-21 10:19:10 +0100

    test(Secrets): fix tests

--- src/app/modules/deployments/components/secret-management-detail/secret-management-detail.component.spec.ts
+++ src/app/modules/deployments/components/secret-management-detail/secret-management-detail.component.spec.ts
@@ -1,23 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SecretManagementDetailComponent } from './secret-management-detail.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AppConfigService } from '@app/core/services/app-config/app-config.service';
+import { SharedModule } from '@app/shared/shared.module';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { TranslateModule } from '@ngx-translate/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+const mockedConfigService: any = {};
+
+const mockedMediaQueryList: MediaQueryList = {
+    matches: true,
+    media: 'test',
+    onchange: jest.fn(),
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+    removeEventListener: jest.fn(),
+};
+
+const mockedMediaMatcher: any = {
+    matchMedia: jest.fn().mockReturnValue(mockedMediaQueryList),
+};
 
 describe('SecretManagementDetailComponent', () => {
-  let component: SecretManagementDetailComponent;
-  let fixture: ComponentFixture<SecretManagementDetailComponent>;
+    let component: SecretManagementDetailComponent;
+    let fixture: ComponentFixture<SecretManagementDetailComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SecretManagementDetailComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SecretManagementDetailComponent],
+            imports: [
+                NoopAnimationsModule,
+                HttpClientTestingModule,
+                SharedModule,
+                TranslateModule.forRoot(),
+            ],
+            providers: [
+                { provide: AppConfigService, useValue: mockedConfigService },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+                { provide: MediaMatcher, useValue: mockedMediaMatcher },
+            ],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(SecretManagementDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(SecretManagementDetailComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/modules/deployments/services/secrets-service/secrets.service.spec.ts
+++ src/app/modules/deployments/services/secrets-service/secrets.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SecretsService } from './secrets.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AppConfigService } from '@app/core/services/app-config/app-config.service';
+
+const mockedConfigService: any = {};
 
 describe('SecretsService', () => {
-  let service: SecretsService;
+    let service: SecretsService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SecretsService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                { provide: AppConfigService, useValue: mockedConfigService },
+            ],
+        });
+        service = TestBed.inject(SecretsService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
