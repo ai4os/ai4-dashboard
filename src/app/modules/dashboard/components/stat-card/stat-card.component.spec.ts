29130ba1eedcff134213dcd3e9291512e8fc6958
Marta Obregón <obregonm@ifca.unican.es>
2024-03-01 10:43:57 +0100

    test: fix tests

--- src/app/modules/dashboard/components/stat-card/stat-card.component.spec.ts
+++ src/app/modules/dashboard/components/stat-card/stat-card.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StatCardComponent } from './stat-card.component';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SharedModule } from '@app/shared/shared.module';
+
+const mockedMediaQueryList: MediaQueryList = {
+    matches: true,
+    media: 'test',
+    onchange: jest.fn(),
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+    removeEventListener: jest.fn(),
+};
+const mockedMediaMatcher: any = {
+    matchMedia: jest.fn().mockReturnValue(mockedMediaQueryList),
+};
 
 describe('StatCardComponent', () => {
     let component: StatCardComponent;
@@ -9,6 +26,11 @@ describe('StatCardComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [StatCardComponent],
+            imports: [SharedModule],
+            providers: [
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+                { provide: MediaMatcher, useValue: mockedMediaMatcher },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(StatCardComponent);
--- src/app/modules/dashboard/components/stats-container/stats-container.component.spec.ts
+++ src/app/modules/dashboard/components/stats-container/stats-container.component.spec.ts
@@ -2,23 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StatsContainerComponent } from './stats-container.component';
 import { TranslateModule } from '@ngx-translate/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SharedModule } from '@app/shared/shared.module';
-import { MediaMatcher } from '@angular/cdk/layout';
-
-const mockedMediaQueryList: MediaQueryList = {
-    matches: true,
-    media: 'test',
-    onchange: jest.fn(),
-    addListener: jest.fn(),
-    removeListener: jest.fn(),
-    addEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-    removeEventListener: jest.fn(),
-};
-const mockedMediaMatcher: any = {
-    matchMedia: jest.fn().mockReturnValue(mockedMediaQueryList),
-};
 
 describe('StatsContainerComponent', () => {
     let component: StatsContainerComponent;
@@ -28,10 +12,6 @@ describe('StatsContainerComponent', () => {
         await TestBed.configureTestingModule({
             declarations: [StatsContainerComponent],
             imports: [TranslateModule.forRoot(), SharedModule],
-            providers: [
-                { provide: MAT_DIALOG_DATA, useValue: {} },
-                { provide: MediaMatcher, useValue: mockedMediaMatcher },
-            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(StatsContainerComponent);
