3ebc665f2066b66b97d68f973eda13cf65ae9e41
Marta Obregón <obregonm@ifca.unican.es>
2024-04-09 10:10:45 +0200

    fix: spinner never ends when get secrets fails

--- src/app/modules/deployments/components/secret-management-detail/secret-management-detail.component.ts
+++ src/app/modules/deployments/components/secret-management-detail/secret-management-detail.component.ts
@@ -74,20 +74,25 @@ export class SecretManagementDetailComponent implements OnInit {
         const subpath = '/deployments/' + this.data.uuid + '/federated/';
         this.secrets = [];
 
-        this.secretsService.getSecrets(subpath).subscribe((secrets) => {
-            for (let i = 0; i < Object.values(secrets).length; i++) {
-                const secret: SecretField = {
-                    name: Object.keys(secrets)[i].substring(
-                        Object.keys(secrets)[i].lastIndexOf('/') + 1
-                    ),
-                    value: Object.values(secrets)[i].token,
-                    hide: true,
-                };
-                this.secrets.push(secret);
-            }
-            this.paginatedSecrets = this.secrets.slice(0, this.pageSize);
-            this.length = this.secrets.length;
-            this.isLoading = false;
+        this.secretsService.getSecrets(subpath).subscribe({
+            next: (secrets) => {
+                for (let i = 0; i < Object.values(secrets).length; i++) {
+                    const secret: SecretField = {
+                        name: Object.keys(secrets)[i].substring(
+                            Object.keys(secrets)[i].lastIndexOf('/') + 1
+                        ),
+                        value: Object.values(secrets)[i].token,
+                        hide: true,
+                    };
+                    this.secrets.push(secret);
+                }
+                this.paginatedSecrets = this.secrets.slice(0, this.pageSize);
+                this.length = this.secrets.length;
+                this.isLoading = false;
+            },
+            error: () => {
+                this.isLoading = false;
+            },
         });
     }
 
