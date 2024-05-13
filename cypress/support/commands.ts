731af0299c722dc41de7255200e7847a77e043c5
Marta Obregón <obregonm@ifca.unican.es>
2024-05-13 13:25:02 +0200

    test: add e2e tests

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -10,34 +10,43 @@ Cypress.Commands.add('login', (username: string, password: string) => {
 
     log.snapshot('before');
     const args = { username, password };
-    cy.session(`auth-${username}`, () => {
-        cy.visit('http://localhost:8080/');
-        cy.contains('Login').click();
+    cy.session(
+        `auth-${username}`,
+        () => {
+            cy.visit('http://localhost:8080/');
+            cy.contains('Login').click();
 
-        cy.origin('https://aai.egi.eu', () => {
-            cy.get('.ssp-btn.bitbucket').click();
-        });
+            cy.origin('https://aai.egi.eu', () => {
+                cy.get('.ssp-btn.bitbucket').click();
+            });
 
-        cy.origin(
-            'https://id.atlassian.com/l',
-            { args },
-            ({ username, password }) => {
-                cy.get('#username').type(username, {
-                    log: false,
-                });
+            cy.origin(
+                'https://id.atlassian.com/l',
+                { args },
+                ({ username, password }) => {
+                    cy.get('#username').type(username, {
+                        log: false,
+                    });
 
-                cy.get('#login-submit').click();
+                    cy.get('#login-submit').click();
 
-                cy.get('#password').type(password, {
-                    log: false,
-                });
+                    cy.get('#password').type(password, {
+                        log: false,
+                    });
 
-                cy.get('#login-submit').click();
-            }
-        );
+                    cy.get('#login-submit').click();
+                }
+            );
 
-        cy.url().should('equal', 'http://localhost:8080/marketplace');
-    });
+            cy.url().should('equal', 'http://localhost:8080/');
+        },
+        {
+            validate() {
+                cy.contains(`Test AI4EOSC`);
+            },
+            cacheAcrossSpecs: true,
+        }
+    );
 
     log.snapshot('after');
 });
