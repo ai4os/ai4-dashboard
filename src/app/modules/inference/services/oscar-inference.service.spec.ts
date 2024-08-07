41761a802408064fd87a2fa189cfccad81c20f54
Marta Obregón <obregonm@ifca.unican.es>
2024-08-07 14:03:16 +0200

    test: make initial config

--- src/app/modules/inference/services/oscar-inference.service.spec.ts
+++ src/app/modules/inference/services/oscar-inference.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OscarInferenceService } from './oscar-inference.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('OscarInferenceService', () => {
     let service: OscarInferenceService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
         service = TestBed.inject(OscarInferenceService);
     });
 
