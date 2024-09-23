1d3746c68c2387b8a4bf26eac2ed0622a53a6175
Marta Obregón <obregonm@ifca.unican.es>
2024-09-23 09:27:35 +0200

    test: adapt tests

--- cypress/e2e/deployments-list.component.cy.ts
+++ cypress/e2e/deployments-list.component.cy.ts
@@ -33,11 +33,11 @@ describe('deployments section', function () {
 
     it('shows secrets management dialog', function () {
         cy.get('#secrets-button').click();
-        cy.contains('Manage secrets', { timeout: 20000 }).should('be.visible');
+        cy.get('#card-title').contains('Manage secrets').should('be.visible');
         cy.get('.tool-title').contains('fl-test').should('be.visible');
         cy.contains('List of secrets').should('be.visible');
         cy.contains('default').should('be.visible');
-        cy.contains('Add').should('be.visible');
+        cy.get('#add-button').contains('Add').should('be.visible');
         cy.contains('Close').should('be.visible');
     });
 
@@ -60,7 +60,7 @@ describe('deployments section', function () {
         cy.contains('Successfully created secret with name: client1').should(
             'be.visible'
         );
-        cy.contains('client1', { timeout: 10000 }).should('be.visible');
+        cy.contains('client1', { timeout: 20000 }).should('be.visible');
     });
 
     it('create duplicated secret', function () {
