6c31c94f637969bb90dfc1b4bbbb2187a5c71ee2
Marta Obregón Ruiz <151519478+MartaOB@users.noreply.github.com>
2024-10-28 11:03:11 +0100

    feat: 227 add differential privacy to fl config (#242)
    * feat: add differential privacy to the federated learning server tool
    
    * fix: lint
    
    * feat: change noise mult step

--- src/app/shared/interfaces/module.interface.ts
+++ src/app/shared/interfaces/module.interface.ts
@@ -151,6 +151,10 @@ export interface TrainModuleRequest {
         mu: number;
         fl: number;
         momentum: number;
+        dp: boolean;
+        noise_mult: number;
+        sampled_clients: number;
+        clip_norm: number;
     };
 }
 
