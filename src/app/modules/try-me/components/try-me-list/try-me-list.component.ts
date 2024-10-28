95681120b3dec8159c6532e23c987cd2f52b2e64
Marta Obregón <obregonm@ifca.unican.es>
2024-10-28 09:14:07 +0100

    fix: lint

--- src/app/modules/try-me/components/try-me-list/try-me-list.component.ts
+++ src/app/modules/try-me/components/try-me-list/try-me-list.component.ts
@@ -101,9 +101,11 @@ export class TryMeListComponent implements OnInit, OnDestroy {
 
     /** Selects all rows if they are not all selected; otherwise clear selection. */
     masterToggle() {
-        this.isAllSelected()
-            ? this.selection.clear()
-            : this.dataSource.data.forEach((row) => this.selection.select(row));
+        if (this.isAllSelected()) {
+            this.selection.clear();
+        } else {
+            this.dataSource.data.forEach((row) => this.selection.select(row));
+        }
     }
 
     returnDeploymentBadge(status: string) {
