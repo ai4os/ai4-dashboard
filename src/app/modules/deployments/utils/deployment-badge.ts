f1898dd95419909eb41530960c6cb6445b4859c3
Marta Obregón <obregonm@ifca.unican.es>
2024-11-21 08:10:30 +0100

    fix: lint

--- src/app/modules/deployments/utils/deployment-badge.ts
+++ src/app/modules/deployments/utils/deployment-badge.ts
@@ -1,32 +1,32 @@
 export function getDeploymentBadge(status: string) {
     let statusBadge;
     switch (status) {
-        case 'pending':
-        case 'scheduled':
-        case 'queued':
-            statusBadge = status + '-fd5d00';
-            break;
-        case 'starting':
-            statusBadge = status + '-yellow';
-            break;
-        case 'in progress':
-            statusBadge = status + '-orange';
-            break;
-        case 'running':
-            statusBadge = status + '-brightgreen';
-            break;
-        case 'failed':
-        case 'error':
-            statusBadge = status + '-red';
-            break;
-        case 'dead':
-        case 'draining':
-        case 'complete':
-            statusBadge = status + '-lightgrey';
-            break;
-        default:
-            statusBadge = 'unknown-lightgrey';
-            break;
+    case 'pending':
+    case 'scheduled':
+    case 'queued':
+        statusBadge = status + '-fd5d00';
+        break;
+    case 'starting':
+        statusBadge = status + '-yellow';
+        break;
+    case 'in progress':
+        statusBadge = status + '-orange';
+        break;
+    case 'running':
+        statusBadge = status + '-brightgreen';
+        break;
+    case 'failed':
+    case 'error':
+        statusBadge = status + '-red';
+        break;
+    case 'dead':
+    case 'draining':
+    case 'complete':
+        statusBadge = status + '-lightgrey';
+        break;
+    default:
+        statusBadge = 'unknown-lightgrey';
+        break;
     }
     return statusBadge;
 }
@@ -34,27 +34,27 @@ export function getDeploymentBadge(status: string) {
 export function getSnapshotBadge(status: string) {
     let statusBadge;
     switch (status) {
-        case 'queued':
-            statusBadge = status + '-fd5d00';
-            break;
-        case 'starting':
-            statusBadge = status + '-yellow';
-            break;
-        case 'in progress':
-            statusBadge = status + '-orange';
-            break;
-        case 'running':
-            statusBadge = status + '-brightgreen';
-            break;
-        case 'error':
-            statusBadge = status + '-red';
-            break;
-        case 'complete':
-            statusBadge = status + '-brightgreen';
-            break;
-        default:
-            statusBadge = 'unknown-lightgrey';
-            break;
+    case 'queued':
+        statusBadge = status + '-fd5d00';
+        break;
+    case 'starting':
+        statusBadge = status + '-yellow';
+        break;
+    case 'in progress':
+        statusBadge = status + '-orange';
+        break;
+    case 'running':
+        statusBadge = status + '-brightgreen';
+        break;
+    case 'error':
+        statusBadge = status + '-red';
+        break;
+    case 'complete':
+        statusBadge = status + '-brightgreen';
+        break;
+    default:
+        statusBadge = 'unknown-lightgrey';
+        break;
     }
     return statusBadge;
 }
