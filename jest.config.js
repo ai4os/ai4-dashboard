901b71bde3d1520edd82c0a699ceb730db7dc012
nocontribute <>
2025-01-14 13:21:53 +0100

    fix: delete unused library and adapt workflows

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,15 @@ module.exports = {
     globalSetup: 'jest-preset-angular/global-setup',
     modulePaths: ['<rootDir>', '/home/some/other/path'],
     moduleNameMapper: {
-        "^~(.*)$": "<rootDir>/src/$1",
-        "@app(.*)": "<rootDir>/src/app/$1",
-        "@core(.*)": "<rootDir>/src/app/core/$1",
-        "@modules(.*)": "<rootDir>/src/app/modules/$1",
-        "@shared(.*)": "<rootDir>/src/app/shared/$1",
-        "@environments(.*)": "<rootDir>/src/environments/$1",
-        "@data(.*)": "<rootDir>/src/app/data/$1",
-        '^@grycap/oscar-js$':
-            '<rootDir>/src/app/modules/marketplace/services/modules-service/oscar-module.mock.ts',
-      },
-      transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|ol|quick-lru|lodash-es|color-(space|parse|rgba|name)|crypto-random-string/)']};
+        '^~(.*)$': '<rootDir>/src/$1',
+        '@app(.*)': '<rootDir>/src/app/$1',
+        '@core(.*)': '<rootDir>/src/app/core/$1',
+        '@modules(.*)': '<rootDir>/src/app/modules/$1',
+        '@shared(.*)': '<rootDir>/src/app/shared/$1',
+        '@environments(.*)': '<rootDir>/src/environments/$1',
+        '@data(.*)': '<rootDir>/src/app/data/$1',
+    },
+    transformIgnorePatterns: [
+        'node_modules/(?!.*\\.mjs$|ol|quick-lru|lodash-es|color-(space|parse|rgba|name)|crypto-random-string/)',
+    ],
+};
