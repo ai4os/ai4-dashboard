2ccdfd9f296148819dcc42237714623cac0ac640
Marta Obregón <obregonm@ifca.unican.es>
2025-02-28 09:46:43 +0100

    test: fix and prepare tests

--- src/app/modules/marketplace/components/module-train/tool-train/llm/llm.component.spec.ts
+++ src/app/modules/marketplace/components/module-train/tool-train/llm/llm.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LlmComponent } from './llm.component';
+import { RouterModule } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AppConfigService } from '@app/core/services/app-config/app-config.service';
+
+const mockedConfigService: any = {};
 
 describe('LlmComponent', () => {
     let component: LlmComponent;
@@ -8,9 +14,14 @@ describe('LlmComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [LlmComponent]
-        })
-            .compileComponents();
+            imports: [RouterModule.forRoot([])],
+            declarations: [LlmComponent],
+            providers: [
+                provideHttpClient(),
+                provideHttpClientTesting(),
+                { provide: AppConfigService, useValue: mockedConfigService },
+            ],
+        }).compileComponents();
 
         fixture = TestBed.createComponent(LlmComponent);
         component = fixture.componentInstance;
