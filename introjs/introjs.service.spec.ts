0bd6acaf8ae1b0b0d6de9394899c67e9bf6e9ead
Marta Obregón Ruiz <151519478+MartaOB@users.noreply.github.com>
2025-03-10 09:52:46 +0100

    fix: show on boarding tours only to logged users (#355)
    * feat: show tour only to logged users
    
    * feat: keep on boarding library related variables in local storage
    
    * test: fix

--- introjs/introjs.service.spec.ts
+++ introjs/introjs.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { IntroJSService } from './introjs.service';
+import { AuthService } from '@app/core/services/auth/auth.service';
+
+const mockedAuthService: any = {
+    isAuthenticated: jest.fn(),
+};
 
 describe('IntrojsService', () => {
     let service: IntroJSService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [{ provide: AuthService, useValue: mockedAuthService }],
+        });
         service = TestBed.inject(IntroJSService);
     });
 
