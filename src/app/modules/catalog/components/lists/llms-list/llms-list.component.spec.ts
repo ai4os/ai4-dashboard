e4ed0d9c666f5d3ad6fb7dcdf385788c4a170a70
Marta Obregón <obregonm@ifca.unican.es>
2025-03-21 13:55:02 +0100

    fix: tests

--- src/app/modules/catalog/components/lists/llms-list/llms-list.component.spec.ts
+++ src/app/modules/catalog/components/lists/llms-list/llms-list.component.spec.ts
@@ -1,6 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LlmsListComponent } from './llms-list.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { AppConfigService } from '@app/core/services/app-config/app-config.service';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { TranslateModule } from '@ngx-translate/core';
+
+const mockedMediaQueryList: MediaQueryList = {
+    matches: true,
+    media: 'test',
+    onchange: jest.fn(),
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+    removeEventListener: jest.fn(),
+};
+
+const mockedMediaMatcher: any = {
+    matchMedia: jest.fn().mockReturnValue(mockedMediaQueryList),
+};
+
+const mockedConfigService: any = {};
 
 describe('LlmsListComponent', () => {
     let component: LlmsListComponent;
@@ -8,9 +30,15 @@ describe('LlmsListComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [LlmsListComponent]
-        })
-            .compileComponents();
+            declarations: [LlmsListComponent],
+            imports: [TranslateModule.forRoot()],
+            providers: [
+                provideHttpClient(),
+                provideHttpClientTesting(),
+                { provide: AppConfigService, useValue: mockedConfigService },
+                { provide: MediaMatcher, useValue: mockedMediaMatcher },
+            ],
+        }).compileComponents();
 
         fixture = TestBed.createComponent(LlmsListComponent);
         component = fixture.componentInstance;
