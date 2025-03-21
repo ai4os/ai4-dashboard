2a64947873d917e6a357a20b7a1ee9b3fdcfc57f
Marta Obregón <obregonm@ifca.unican.es>
2025-03-21 13:37:05 +0100

    fix: lint

--- src/app/modules/catalog/components/module-train/stepper-form/stepper-form.component.ts
+++ src/app/modules/catalog/components/module-train/stepper-form/stepper-form.component.ts
@@ -101,8 +101,8 @@ export class StepperFormComponent implements OnInit {
                 title:
                     this.step1Form.value.generalConfForm.titleInput === ''
                         ? uniqueNamesGenerator({
-                              dictionaries: [colors, animals],
-                          })
+                            dictionaries: [colors, animals],
+                        })
                         : this.step1Form.value.generalConfForm.titleInput,
                 desc: this.step1Form.value.generalConfForm.descriptionInput,
                 co2: this.step1Form.value.generalConfForm.co2EmissionsInput,
@@ -193,7 +193,7 @@ export class StepperFormComponent implements OnInit {
                             .strategyOptionsSelect ===
                         'Federated Averaging with Momentum (FedAvgM)'
                             ? this.step3Form!.value.federatedConfForm
-                                  .momentumInput
+                                .momentumInput
                             : null,
                     dp: this.step3Form!.value.federatedConfForm.dpInput,
                     mp: this.step3Form!.value.federatedConfForm.dpInput
@@ -205,11 +205,11 @@ export class StepperFormComponent implements OnInit {
                     sampled_clients: this.step3Form!.value.federatedConfForm
                         .dpInput
                         ? this.step3Form!.value.federatedConfForm
-                              .sampledClientsNumInput
+                            .sampledClientsNumInput
                         : null,
                     clip_norm: this.step3Form!.value.federatedConfForm.dpInput
                         ? this.step3Form!.value.federatedConfForm
-                              .clippingNormInput
+                            .clippingNormInput
                         : null,
                 };
                 request = this.deploymentsService.trainTool(
@@ -240,7 +240,7 @@ export class StepperFormComponent implements OnInit {
                             ?.doi === ''
                             ? []
                             : this.step3Form!.value.storageConfForm
-                                  .datasetsList,
+                                .datasetsList,
                 };
 
                 if (this.title === 'AI4OS Development Environment') {
