5e9894cbb286d42b0bb280ff05d1a4dee32dfe90
Marta Obregón <obregonm@ifca.unican.es>
2025-03-24 11:34:35 +0100

    test: fix

--- src/app/modules/marketplace/components/module-train/tool-train/nvflare/nvflare-conf-form/nvflare-conf-form.component.spec.ts
+++ src/app/modules/marketplace/components/module-train/tool-train/nvflare/nvflare-conf-form/nvflare-conf-form.component.spec.ts
@@ -1,16 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NvflareConfFormComponent } from './nvflare-conf-form.component';
+import { AuthService } from '@app/core/services/auth/auth.service';
+import { of } from 'rxjs';
+import { SharedModule } from '@app/shared/shared.module';
+import { FormBuilder, FormGroupDirective } from '@angular/forms';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { TranslateModule } from '@ngx-translate/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const mockedAuthService: any = {
+    isAuthenticated: jest.fn(),
+    userProfileSubject: of({}),
+};
+
+const mockedMediaQueryList: MediaQueryList = {
+    matches: true,
+    media: 'test',
+    onchange: jest.fn(),
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+    removeEventListener: jest.fn(),
+};
+
+const mockedMediaMatcher: any = {
+    matchMedia: jest.fn().mockReturnValue(mockedMediaQueryList),
+};
 
 describe('NvflareConfFormComponent', () => {
     let component: NvflareConfFormComponent;
     let fixture: ComponentFixture<NvflareConfFormComponent>;
+    const fb = new FormBuilder();
+    const formGroupDirective = new FormGroupDirective([], []);
+    formGroupDirective.form = fb.group({
+        test: fb.control(null),
+    });
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [NvflareConfFormComponent]
-        })
-            .compileComponents();
+            imports: [
+                SharedModule,
+                BrowserAnimationsModule,
+                TranslateModule.forRoot(),
+            ],
+            declarations: [NvflareConfFormComponent],
+            providers: [
+                FormGroupDirective,
+                FormBuilder,
+                { provide: FormGroupDirective, useValue: formGroupDirective },
+                { provide: AuthService, useValue: mockedAuthService },
+                { provide: MediaMatcher, useValue: mockedMediaMatcher },
+            ],
+        }).compileComponents();
 
         fixture = TestBed.createComponent(NvflareConfFormComponent);
         component = fixture.componentInstance;
--- src/app/modules/marketplace/components/module-train/tool-train/nvflare/nvflare.component.spec.ts
+++ src/app/modules/marketplace/components/module-train/tool-train/nvflare/nvflare.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NvflareComponent } from './nvflare.component';
+import { RouterModule } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AppConfigService } from '@app/core/services/app-config/app-config.service';
+
+const mockedConfigService: any = {};
 
 describe('NvflareComponent', () => {
     let component: NvflareComponent;
@@ -8,9 +14,14 @@ describe('NvflareComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [NvflareComponent]
-        })
-            .compileComponents();
+            imports: [RouterModule.forRoot([])],
+            declarations: [NvflareComponent],
+            providers: [
+                provideHttpClient(),
+                provideHttpClientTesting(),
+                { provide: AppConfigService, useValue: mockedConfigService },
+            ],
+        }).compileComponents();
 
         fixture = TestBed.createComponent(NvflareComponent);
         component = fixture.componentInstance;
