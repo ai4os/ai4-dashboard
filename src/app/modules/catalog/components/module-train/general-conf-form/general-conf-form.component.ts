a87aa7d13714fcb7d0986f2b5a34afdb5170fe82
Marta Obregón <obregonm@ifca.unican.es>
2025-04-21 09:46:37 +0200

    fix: update fields when model changes (llm train)

--- src/app/modules/catalog/components/module-train/general-conf-form/general-conf-form.component.ts
+++ src/app/modules/catalog/components/module-train/general-conf-form/general-conf-form.component.ts
@@ -241,6 +241,7 @@ export class GeneralConfFormComponent implements OnInit {
                     });
                 }
             );
+            this.modelChanged();
 
             this.generalConfFormGroup
                 .get('uiPasswordInput')
