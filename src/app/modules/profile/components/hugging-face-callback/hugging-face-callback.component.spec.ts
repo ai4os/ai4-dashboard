df6fa03fb260156872988b58417022b74faf104d
Marta Obregón <obregonm@ifca.unican.es>
2025-04-21 09:05:08 +0200

    fix: lint

--- src/app/modules/profile/components/hugging-face-callback/hugging-face-callback.component.spec.ts
+++ src/app/modules/profile/components/hugging-face-callback/hugging-face-callback.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HuggingFaceCallbackComponent } from './hugging-face-callback.component';
 
 describe('HuggingFaceCallbackComponent', () => {
-  let component: HuggingFaceCallbackComponent;
-  let fixture: ComponentFixture<HuggingFaceCallbackComponent>;
+    let component: HuggingFaceCallbackComponent;
+    let fixture: ComponentFixture<HuggingFaceCallbackComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HuggingFaceCallbackComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HuggingFaceCallbackComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(HuggingFaceCallbackComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(HuggingFaceCallbackComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
