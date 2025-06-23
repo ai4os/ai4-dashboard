57470278f15ed2213017350c38066a8f000a7199
Marta Obregón <obregonm@ifca.unican.es>
2025-06-23 10:00:37 +0200

    ci: remove cypress plugin

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,9 +1,1 @@
 import './commands';
-
-require('cypress-terminal-report/src/installLogsPrinter')({
-    printLogsToConsole: 'always',
-    outputRoot: './logs/',
-    outputTarget: {
-        'cypress.log': 'txt',
-    },
-});
