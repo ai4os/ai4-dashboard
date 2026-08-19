8aa0b534e48af910985a7067f777a124f073a6de
Marta Obregón Ruiz <obregonm@ifca.es>
2026-08-19 08:45:47 +0200

    fix: sonarqube analysis

--- src/app/shared/components/ui/ui-button/ui-button.component.ts
+++ src/app/shared/components/ui/ui-button/ui-button.component.ts
@@ -24,6 +24,7 @@ export class UiButtonComponent implements OnInit {
         | 'danger-solid'
         | 'danger-outline'
         | 'icon' = 'primary-solid';
+
     @Input() icon?: string;
     @Input() text?: string;
     @Input() disabled = false;
@@ -38,9 +39,11 @@ export class UiButtonComponent implements OnInit {
         }
     }
 
-    onClick() {
-        if (this.href) {
-            window.open(this.href, '_blank', 'noopener,noreferrer');
-        }
+    onClick(): void {
+        // Action button behaviour
+    }
+
+    onLinkClick(event: MouseEvent): void {
+        event.stopPropagation();
     }
 }
